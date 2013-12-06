097f78ea8a1ed2c521cbcbe805ef685e06d10e16
supermomonga <hi@supermomonga.com>
2013-12-06 17:00:49 +0900

    initial

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'sinatra'
+gem 'shotgun'
+gem 'slim'
+gem 'sass'
+gem 'coffee-script'
---
+++ app.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+require 'bundler'
+Bundler.require
+
+
+get '/css/*.css' do |name|
+  scss name.to_sym
+end
+
+get '/js/*.js' do |name|
+  coffee name.to_sym
+end
+
+get '/' do
+  slim :index
+end
+
+get '/*.html' do |name|
+  slim name.to_sym
+end
