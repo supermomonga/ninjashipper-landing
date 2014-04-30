165578b758ddbfa9497ee83e4523f34f524d3c89
supermomonga <hi@supermomonga.com>
2014-04-30 10:58:24 +0900

    add business partnership / others

--- web.rb
+++ web.rb
@@ -14,15 +14,18 @@ get '/js/*.js' do |name|
 end
 
 get '/' do
+  @page = :index
   # slim :index, layout: false
   slim :comming_soon, layout: false
 end
 
 get '/*.html' do |name|
   if name == 'index'
+    @page = name.to_sym
     slim :comming_soon, layout: false
   else
     name = :index if name == 'index_tmp'
+    @page = name.to_sym
     slim name.to_sym
   end
 end
