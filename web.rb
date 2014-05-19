b049227347be05b3727d391546a69c94794984ac
supermomonga <hi@supermomonga.com>
2014-05-20 08:38:06 +0900

    recommended shops

--- web.rb
+++ web.rb
@@ -3,6 +3,9 @@
 require 'bundler'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'csv'
+require 'erb'
+require 'cgi'
 
 
 get '/css/*.css' do |name|
@@ -25,6 +28,27 @@ end
 
 before do
   @debug = false
+end
+
+get '/shops.html' do
+  @page_title = "Our recommended shops"
+  @cat_id = params[:cat] || ERB::Util.url_encode('Our shops')
+  @cat_name = CGI::unescape @cat_id
+
+  data = CSV.table('./data/shops.csv')
+  @categories = data[:category].inject({}){|acc,e|
+    acc[e] ||= {
+      name: e,
+      id: ERB::Util.url_encode(e),
+      count: 0
+    }
+    acc[e][:count] += 1
+    acc
+  }.values
+  @shops = data.select{|d| d[:category] == @cat_name }
+  p @shops.first
+
+  slim :shops
 end
 
 get '/*.html' do |name|
